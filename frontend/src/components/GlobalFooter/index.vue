dfca1badcf9bdd4a04bb33027cb8b18e8b51d8e5
Jing Ling <shmilylty@gmail.com>
2020-10-21 16:01:18 +0800

    Update index.vue

--- frontend/src/components/GlobalFooter/index.vue
+++ frontend/src/components/GlobalFooter/index.vue
@@ -1,13 +1,5 @@
 <template>
   <global-footer class="footer custom-render">
-    <template v-slot:links>
-      <a href="https://github.com/chennqqi/godnslog" target="_blank">Wechat</a>
-      <a href="https://github.com/chennqqi/godnslog" target="_blank">Github</a>
-      <a href="https://github.com/chennqqi/" target="_blank">@chennqqi</a>
-    </template>
-    <template v-slot:copyright>
-      <a href="https://www.shellpub.com" target="_blank">SHELLPUB.COM</a>
-    </template>
   </global-footer>
 </template>
 
